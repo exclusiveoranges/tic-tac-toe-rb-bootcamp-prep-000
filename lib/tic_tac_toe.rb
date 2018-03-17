f73e6f9674406d674ca81b1806c11e56748ac3af
Ryan O'Toole <otoolerj@gmail.com>
2018-03-17 20:56:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,3 @@
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -81,12 +77,12 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
 def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+  board[location] != " " && board[location] != "" && board[location] != nil
 end
 
 def valid_move?(board, index)
@@ -98,9 +94,22 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
+  end
+end
+
+def play(board)
+  turn(board)
+  if over?(board) == false
+    play(board)
+  elsif winner?(board) == "X"
+    puts "Congratulations X"
+  elsif winner?(board) == "O"
+    puts "Congratulations O"
+  else draw?(board) == true
+    puts "Cat's Game!"
   end
 end
